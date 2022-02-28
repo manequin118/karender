5444e63738004724b915548f2e0b1fb83a350570
manequin118 <manequin118@gmail.com>
2022-03-01 00:58:05 +0900

    crud処理途中まで実装

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
+include 'connect.php';
+include 'queryShcedule.php';
+include 'shcedule.php';
 
-
+$queryShcedule = new QueryShcedule();
+$shcedules = $queryShcedule->findAll();
 
 
 ?>
@@ -16,7 +20,36 @@
 </head>
 
 <body>
-
+  <div class="shcedule-container">
+    <?php if ($shcedules) : ?>
+      <table class="table ">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>タイトル</th>
+            <th>本文</th>
+            <th>日程</th>
+            <th>ボタン</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($shcedules as $shcedule) : ?>
+            <tr>
+              <td><?php echo $shcedule->getId() ?></td>
+              <td><?php echo $shcedule->getTitle() ?></td>
+              <td><?php echo $shcedule->getBody() ?></td>
+              <td><?php echo $shcedule->getStudyDay() ?></td>
+              <td><a href="edit.php?id=<?php echo $shcedule->getId() ?>" class="btn btn-success">編集</a></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+    <?php else : ?>
+      <div class="alert alert-info">
+        <p>記事はありません。</p>
+      </div>
+    <?php endif; ?>
+  </div>
 </body>
 
 </html>
--- post.php
+++ post.php
@@ -1,26 +1,49 @@
 <?php
 include 'connect.php';
 include "shcedule.php";
-include "QueryShcedule.php";
+include "queryShcedule.php";
 
+$id = "";
 $title = "";        // タイトル
 $body = "";         // 本文
-$study_day = "";         //日付
+$study_day = "";    //日付
 $title_alert = "";  // タイトルのエラー文言
 $body_alert = "";   // 本文のエラー文言
-$study_day = "";     //日にちのエラー文
 
-if (!empty($_POST['id']) && !empty($_POST['title']) && !empty($_POST['body']) && !empty($_POST['study_day'])) {
+
+if (!empty($_POST['title']) && !empty($_POST['body']) && !empty($_POST['study_day'])) {
   // id, titleとbodyがPOSTメソッドで送信されたとき
-  $title = filter_input(INPUT_POST, "title");
-  $body = filter_input(INPUT_POST, "body");
-  $study_day = filter_input(INPUT_POST, "study_day");
+  $title = $_POST["title"];
+  $body = $_POST["body"];
+  $study_day = $_POST["study_day"];
+
+  // $db = new connect();
+  // $sql = "INSERT INTO shcedule (title, body,study_day, created_at, updated_at)
+  //           VALUES (:title, :body,:study_day, NOW(), NOW())";
+  // $result = $db->query($sql, array(':title' => $title, ':body' => $body, ":study_day" => $study_day));
+
+  // header('Location: index.php');
 
   $shcedule = new Shcedule();
   $shcedule->setTitle($title);
   $shcedule->setBody($body);
   $shcedule->setStudyDay($study_day);
   $shcedule->save();
+  header('Location: index.php');
+} else if (!empty($_POST)) {
+  // POSTメソッドで送信されたが、titleかbodyが足りないとき
+  // 存在するほうは変数へ、ない場合空文字にしてフォームのvalueに設定する
+  if (!empty($_POST['title'])) {
+    $title = $_POST['title'];
+  } else {
+    $title_alert = "タイトルを入力してください。";
+  }
+
+  if (!empty($_POST['body'])) {
+    $body = $_POST['body'];
+  } else {
+    $body_alert = "本文を入力してください。";
+  }
 }
 ?>
 <!DOCTYPE html>
@@ -37,11 +60,13 @@ if (!empty($_POST['id']) && !empty($_POST['title']) && !empty($_POST['body']) &&
 <body>
   <div class="post-container">
     <div class="post-contents">
-      <form action="edit.php" method="post">
+      <form action="post.php" method="post">
         <label for="">タイトル</label>
-        <input type="text" name="title">
+        <?php echo !empty($title_alert) ?  $title_alert  : '' ?>
+        <input type="text" name="title" value="<?php echo $title; ?>">
         <label for="">内容</label>
-        <input type="text" name="body">
+        <?php echo !empty($body_alert) ?  $body_alert : '' ?>
+        <input type="text" name="body" value="<?php echo $body; ?>">
         <label for="">日程</label>
         <input type="date" name="study_day">
         <button type="submit">送信する</button>
